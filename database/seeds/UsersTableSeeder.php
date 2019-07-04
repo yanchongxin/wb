182dd5a314fe8a0c17ee7504f8c254ed9d6b81dc
windowsname <windows>
2019-07-04 16:27:10 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '123123';
         $user->email = '123123@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 
